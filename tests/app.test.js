30d4ee49bca17582487c1672af241cc96dc47584
Sriman <sriman@yoloai.pro>
2026-09-10 07:13:19 +0000

    Test case for CICD pipeline

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,6 +1,7 @@
 const request = require("supertest");
 const app = require("../src/app");
 
+
 describe("GoPrimo CI/CD Demo App", () => {
   test("GET / should return app status", async () => {
     const response = await request(app).get("/");
